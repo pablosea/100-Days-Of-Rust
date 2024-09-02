6d22c18ca2c14509ac39df3c81600fec9e34e7d7
pablo <cipollapauljr@gmail.com>
2024-09-01 23:01:08 -0400

    array refrence update, e man was right

--- Week-01/Day-04_Is-Johnny-Making-Progress/johnny_miles/src/main.rs
+++ Week-01/Day-04_Is-Johnny-Making-Progress/johnny_miles/src/main.rs
@@ -1,15 +1,20 @@
 fn main() {
-    progress_days(vec![3, 4, 1, 2]);
-    progress_days(vec![10, 11, 12, 9, 10]);
-    progress_days(vec![6, 5, 4, 3, 2, 9]);
-    progress_days(vec![9, 9]);
+    progress_days(&[3, 4, 1, 2]);
+    progress_days(&[10, 11, 12, 9, 10]);
+    progress_days(&[6, 5, 4, 3, 2, 9]);
+    progress_days(&[9, 9]);
 }
 
-fn progress_days(miles: Vec<i32>){
+fn progress_days(miles: &[i32]){
     let mut prog_day: i32 =0;
     for (x,i) in miles.iter().enumerate(){
         if x > 0{
-            if i > &miles[x-1]{
+            //Original code line: if i > miles[x-1]{
+            //A friend writing the same program got suggested "help: consider dereferencing the borrow: if *i > miles[x-1]{"
+            //I got suggested "consider borrowing here: if i > &miles[x-1] "
+            //Why does derefrenced "i" work just the same as refrenced "miles"? 
+            //Why does rust complier not suggest the same thing?
+            if *i > miles[x-1]{
                 prog_day +=1;
             }
         }
